680775135adadd232dcb4271b06496d709c5c95e
Atoti2 <bogyikormi3@gmail.com>
2024-12-05 16:40:45 +0100

    upload file

--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -1,15 +1,18 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const uploadFile = async (file) => {  
-    const formData = new FormData()
-    formData.append('file', file)
-    formData.append('upload_preset', import.meta.VITE_CLOUDINARY_UPLOAD_PRESET)
-    
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+
     try {
-        const response = await axios.post(`https://api.cloudinary.com/v1_v1/${import.meta.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, formData)
-        return response.data.secure_url
+        const response = await axios.post(
+            `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+            formData
+        );
+        return response.data.secure_url;
     } catch (error) {
-        console.log(error);
-        
+        console.error("File upload failed:", error);
+        throw error; 
     }
-}
+};
