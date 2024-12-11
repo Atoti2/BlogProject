bfb207af3026a2629ae2a5f4cff4ffddf8939a90
Your Name <you@example.com>
2024-12-11 14:36:11 +0100

    filter

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import Post from '../components/Post';
+import { useContext } from 'react';
+import { CategContext } from '../context/CategContext';
 
 const Posts = () => {
   let params = useParams();
-
+  const { categories } = useContext(CategContext)
   useEffect(() => {
     window.scrollTo(0, 0); 
   }, []);
@@ -31,6 +33,15 @@ const Posts = () => {
   return (
     <div className='min-h-screen'>
       <div className='flex gap-10 justify-center flex-col m-10 mt-40'>
+      <div className="flex gap-10 m-auto flex-col sm:flex-row">
+        {categories.map((category) => (
+          <div className='bg-slate-100/50 p-1 rounded-md flex items-center gap-10'>
+          <span className="label-text">{category.name}</span>
+          <input type="checkbox" defaultChecked className="checkbox" />
+          
+          </div>
+        ))}
+      </div>
         {filteredPosts.map((title) => 
           <Post key={title.title} title={title.title} desc={title.desc}/>
         )}
